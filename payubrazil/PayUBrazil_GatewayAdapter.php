c1f72cefa3b1c2ef5ebbdad8e873fc296d561c90
Gui Rams <guirams@gmail.com>
2017-03-31 17:40:49 -0300

    OCD identation fix

--- payubrazil/PayUBrazil_GatewayAdapter.php
+++ payubrazil/PayUBrazil_GatewayAdapter.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+
 namespace Commerce\Gateways\Omnipay;
 
 use Commerce\Gateways\PaymentFormModels\PayUBrazilPaymentFormModel;
@@ -6,68 +7,64 @@ use Omnipay\Common\Message\AbstractRequest as OmnipayRequest;
 use Craft\AttributeType;
 use Craft\BaseModel;
 
-
-class PayUBrazil_GatewayAdapter extends \Commerce\Gateways\CreditCardGatewayAdapter
+class PayUBrazil_GatewayAdapter extends \Commerce\Gateways\CreditCardGatewayAdapter 
 {
-    public function handle()
+    public function handle() 
     {
         return "PayUBrazil";
     }
-
-    public function getPaymentFormModel()
-	{
-		return new PayUBrazilPaymentFormModel();
-	}
-
-    public function populateRequest(OmnipayRequest $request, BaseModel $paymentForm)
+    
+    public function getPaymentFormModel() 
     {
-        if ($paymentForm->paymentMethod)
+        return new PayUBrazilPaymentFormModel();
+    }
+    
+    public function populateRequest(OmnipayRequest $request, BaseModel $paymentForm) 
+    {
+        if ($paymentForm->paymentMethod) 
         {
             $request->setPaymentMethod($paymentForm->paymentMethod);
-        }        
-		
-		if ($paymentForm->holderDocumentNumber)
+        }
+
+        if ($paymentForm->holderDocumentNumber) 
         {
             $request->setHolderDocumentNumber($paymentForm->holderDocumentNumber);
-        }        
-		
-		if ($paymentForm->holderBusinessNumber)
+        }
+
+        if ($paymentForm->holderBusinessNumber) 
         {
             $request->setHolderBusinessNumber($paymentForm->holderBusinessNumber);
         }
 
-        if ($paymentForm->installments)
+        if ($paymentForm->installments) 
         {
             $request->setInstallments($paymentForm->installments);
         }
 
-        if ($paymentForm->token)
+        if ($paymentForm->token) 
         {
             $request->setToken($paymentForm->token);
         }
     }
 
-	public function defineAttributes()
-	{
-		// In addition to the standard gateway config, here is some custom config that is useful.
-        $attr = array();
-		$attr['apiKey'] = [AttributeType::String];
-		$attr['apiKey']['label'] = $this->generateAttributeLabel('apiKey');
-        $attr['publicKey'] = [AttributeType::String];
-		$attr['publicKey']['label'] = $this->generateAttributeLabel('publicKey');
-		$attr['apiLogin'] = [AttributeType::String];
-		$attr['apiLogin']['label'] = $this->generateAttributeLabel('apiLogin');
-		$attr['merchantId'] = [AttributeType::String];
-		$attr['merchantId']['label'] = $this->generateAttributeLabel('merchantId');
-		$attr['accountId'] = [AttributeType::String];
-		$attr['accountId']['label'] = $this->generateAttributeLabel('accountId');
-		$attr['boletoDaysToExpire'] = [AttributeType::Number];
-		$attr['boletoDaysToExpire']['label'] = $this->generateAttributeLabel('boletoDaysToExpire');
-		$attr['testMode'] = [AttributeType::Bool];
-		$attr['testMode']['label'] = $this->generateAttributeLabel('testMode');
-        
+    public function defineAttributes() {
 
-		return $attr;
-	}
-    
+        $attr=array();
+        $attr['apiKey']=[AttributeType: :String];
+        $attr['apiKey']['label']=$this->generateAttributeLabel('apiKey');
+        $attr['publicKey']=[AttributeType: :String];
+        $attr['publicKey']['label']=$this->generateAttributeLabel('publicKey');
+        $attr['apiLogin']=[AttributeType: :String];
+        $attr['apiLogin']['label']=$this->generateAttributeLabel('apiLogin');
+        $attr['merchantId']=[AttributeType: :String];
+        $attr['merchantId']['label']=$this->generateAttributeLabel('merchantId');
+        $attr['accountId']=[AttributeType: :String];
+        $attr['accountId']['label']=$this->generateAttributeLabel('accountId');
+        $attr['boletoDaysToExpire']=[AttributeType: :Number];
+        $attr['boletoDaysToExpire']['label']=$this->generateAttributeLabel('boletoDaysToExpire');
+        $attr['testMode']=[AttributeType: :Bool];
+        $attr['testMode']['label']=$this->generateAttributeLabel('testMode');
+
+        return $attr;
+    }
 }
