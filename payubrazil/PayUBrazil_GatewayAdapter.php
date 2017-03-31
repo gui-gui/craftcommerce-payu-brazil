13ced7d67f286961d52bd5e94c1cea97d46932f0
Gui Rams <guirams@gmail.com>
2017-03-31 17:42:04 -0300

    OCD identation fix again

--- payubrazil/PayUBrazil_GatewayAdapter.php
+++ payubrazil/PayUBrazil_GatewayAdapter.php
@@ -47,23 +47,23 @@ class PayUBrazil_GatewayAdapter extends \Commerce\Gateways\CreditCardGatewayAdap
         }
     }
 
-    public function defineAttributes() {
-
-        $attr=array();
-        $attr['apiKey']=[AttributeType: :String];
-        $attr['apiKey']['label']=$this->generateAttributeLabel('apiKey');
-        $attr['publicKey']=[AttributeType: :String];
-        $attr['publicKey']['label']=$this->generateAttributeLabel('publicKey');
-        $attr['apiLogin']=[AttributeType: :String];
-        $attr['apiLogin']['label']=$this->generateAttributeLabel('apiLogin');
-        $attr['merchantId']=[AttributeType: :String];
-        $attr['merchantId']['label']=$this->generateAttributeLabel('merchantId');
-        $attr['accountId']=[AttributeType: :String];
-        $attr['accountId']['label']=$this->generateAttributeLabel('accountId');
-        $attr['boletoDaysToExpire']=[AttributeType: :Number];
-        $attr['boletoDaysToExpire']['label']=$this->generateAttributeLabel('boletoDaysToExpire');
-        $attr['testMode']=[AttributeType: :Bool];
-        $attr['testMode']['label']=$this->generateAttributeLabel('testMode');
+    public function defineAttributes() 
+    {
+        $attr = array();
+        $attr['apiKey'] = [AttributeType::String];
+        $attr['apiKey']['label'] = $this->generateAttributeLabel('apiKey');
+        $attr['publicKey'] = [AttributeType::String];
+        $attr['publicKey']['label'] = $this->generateAttributeLabel('publicKey');
+        $attr['apiLogin'] = [AttributeType::String];
+        $attr['apiLogin']['label'] = $this->generateAttributeLabel('apiLogin');
+        $attr['merchantId'] = [AttributeType::String];
+        $attr['merchantId']['label'] = $this->generateAttributeLabel('merchantId');
+        $attr['accountId'] = [AttributeType::String];
+        $attr['accountId']['label'] = $this->generateAttributeLabel('accountId');
+        $attr['boletoDaysToExpire'] = [AttributeType::Number];
+        $attr['boletoDaysToExpire']['label'] = $this->generateAttributeLabel('boletoDaysToExpire');
+        $attr['testMode'] = [AttributeType::Bool];
+        $attr['testMode']['label'] = $this->generateAttributeLabel('testMode');
 
         return $attr;
     }
